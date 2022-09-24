da7f88bfef9a987fd8dfa3f15ebc428a821a2b6b
CloverGameSpot1 <114233600+CloverGameSpot1@users.noreply.github.com>
2022-09-24 22:36:13 +0800

    Update setting.js

--- setting.js
+++ setting.js
@@ -10,13 +10,13 @@ global.Api = {
 global.ApiKey = 'coderevolution' //JIKA INGIN MENDAPATKAN KOMISI DAFTAR DI HTTPS://AMPIBISMM.MY.ID UNTUK MENDAPATKAN APIKEY!
 
 global.smm = {
-  nomor : '6283102650464',
-  atasnama : 'Ahmad Nur Fadhil'
+  nomor : '6285158121779',
+  atasnama : 'Muhammad Yusuf Julriyansah'
 }
 
 // Other
-global.owner = ['6283102650464',"6282135724604"]
-global.premium = ['6283102650464']
+global.owner = ['6285158121779',"6285158121779"]
+global.premium = ['6285158121779']
 global.sponsor = 'no sponsors in:('
 global.packname = 'Crizzy-Bot'
 global.footer = '© 2022 Fadhil Graphy'
